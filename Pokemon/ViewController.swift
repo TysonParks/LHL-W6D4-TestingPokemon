038d30f8e8fe3e8d369ac78c05d08e11f543331d
Sam Meech-Ward <sam@meech-ward.com>
2018-02-20 20:38:26 -0800

    Request pokemon and display them in a table view

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -10,9 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var pokemons: [Pokemon] = []
+  
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    var componenets = URLComponents()
+    componenets.scheme = "https"
+    componenets.host = "pokeapi.co"
+    componenets.path = "/api/v2/pokemon/"
+    
+    guard let url = componenets.url else {
+      return
+    }
+    
+    let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+    let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
+      if let error = error {
+        print("Error: \(error)")
+        return
+      }
+      guard let data = data else {
+        print("Error getting data")
+        return
+      }
+      
+      var jsonObject: Any!
+      do {
+        jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+      } catch let error {
+        print("Error: \(error)")
+        return
+      }
+      
+      guard let results = (jsonObject as? [String: Any])?["results"] as? [[String: String]] else {
+        print("Data Error")
+        return
+      }
+      
+      for result in results {
+        guard let name = result["name"], let url = result["url"] else {
+          continue
+        }
+        let pokemon = Pokemon(name: name, url: url)
+        self.pokemons.append(pokemon)
+      }
+      
+      OperationQueue.main.addOperation {
+        self.tableView.reloadData()
+      }
+      
+    }
+    dataTask.resume()
   }
 
   override func didReceiveMemoryWarning() {
@@ -23,3 +75,18 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pokemons.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    
+    let pokemon = pokemons[indexPath.row]
+    cell.textLabel?.text = pokemon.name
+    
+    return cell
+  }
+}
