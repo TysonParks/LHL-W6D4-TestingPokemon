a572537c023b686eede9bdb2e962586f68fb8773
Sam Meech-Ward <sam@meech-ward.com>
2018-02-21 10:45:58 -0800

    Refactor the networking code into different files

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -18,57 +18,20 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    let endpoint = "pokemon"
-    var componenets = URLComponents()
-    componenets.scheme = "https"
-    componenets.host = "pokeapi.co"
-    var componentsURL = componenets.url
-    componentsURL = componentsURL?.appendingPathComponent("api")
-    componentsURL = componentsURL?.appendingPathComponent("v2")
-    componentsURL = componentsURL?.appendingPathComponent(endpoint)
-    
-    guard let url = componentsURL else {
-      return
-    }
-    
-    let urlSession = URLSession(configuration: URLSessionConfiguration.default)
-    let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
+    let pokemonAPI = PokemonAPIRequest(networker: NetworkManager())
+    pokemonAPI.getAllPokemon { (pokemons, error) in
       if let error = error {
         print("Error: \(error)")
+      }
+      guard let pokemons = pokemons else {
+        print("Error getting pokemon")
         return
       }
-      guard let data = data else {
-        print("Error getting data")
-        return
-      }
-      
-      var jsonObject: Any!
-      do {
-        jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-      } catch let error {
-        print("Error: \(error)")
-        return
-      }
-      
-      guard let results = (jsonObject as? [String: Any])?["results"] as? [[String: String]] else {
-        print("Data Error")
-        return
-      }
-      
-      for result in results {
-        guard let name = result["name"], let url = result["url"] else {
-          continue
-        }
-        let pokemon = Pokemon(name: name, url: url)
-        self.pokemons.append(pokemon)
-      }
-      
+      self.pokemons = pokemons
       OperationQueue.main.addOperation {
         self.tableView.reloadData()
       }
-      
     }
-    dataTask.resume()
   }
 
   override func didReceiveMemoryWarning() {
